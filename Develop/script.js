94392b37d133f8fcef9731779f7d3c3490e7c5be
Megan H <megannh6349@gmail.com>
2020-03-20 22:35:43 -0400

    added all prompts

---
+++ Develop/script.js
@@ -0,0 +1,42 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+function generatePassword() {
+  var passLength = prompt("How many characters would you like your password to be? Please enter a number between 8 and 128.");
+  if (passLength > 7 && passLength < 128) {
+    console.log(passLength);
+  } else if (passLength < 8) {
+    alert("Not a valid selection. Please enter a number between 8-128.");
+    //ADD LOOP TO RE-ENTER SELECTION
+  } else if (passLength > 128) {
+    alert("Not a valid selection. Please enter a number between 8-128.");
+    //ADD LOOP TO RE-ENTER SELECTION
+  }
+  var lowercase = confirm("Would you like to use lowercase letters?");
+  if (lowercase === true) {
+    console.log("yes");
+  } else if (lowercase === false) {
+    console.log("no");
+  }
+  var uppercase = confirm("Would you like to use uppercase letters?");
+  if (uppercase === true) {
+    console.log("yes");
+  } else if (uppercase === false) {
+    console.log("no");
+  }
+  var specialChar = confirm("Would you like to use special characters?");
+  if (specialChar === true) {
+    console.log("yes");
+  } else if (specialChar === false) {
+    console.log("no");
+  }
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
