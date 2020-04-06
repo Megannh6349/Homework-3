0c8ed4e85250526deb29ac6927ddaf8a58cc0f0f
Megan H <megannh6349@gmail.com>
2020-04-06 03:13:38 -0400

    adjustments to js

--- script.js
+++ script.js
@@ -1,65 +1,134 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Defining variables to put into array
+var numbersDef = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var lowercaseDef = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+var uppercaseDef = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
+var specialCharDef = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
-function generatePassword(charset, passLength) {
-  var passLength = prompt("How many characters would you like your password to be? Please enter a number between 8 and 128.");
-  if (passLength > 7 && passLength < 129) {
-    console.log(passLength);
-  } else if (passLength < 8) {
-    alert("Not a valid selection. Please enter a number between 8-128.");
+// Function to prompt user for password options
+function getPasswordOptions() {
+  // Variable to store length of password from user input
+  var passLength = parseInt(
+    prompt('How many characters would you like your password to contain?')
+  );
+  // Conditional statement to check if password length is a number. Prompts end if this evaluates false
+  if (isNaN(passLength) === true) {
+    alert('Password length must be provided as a number');
     return;
-    //ADD LOOP TO RE-ENTER SELECTION
-  } else if (passLength > 128) {
-    alert("Not a valid selection. Please enter a number between 8-128.");
-    return;
-    //ADD LOOP TO RE-ENTER SELECTION
   }
-  var numbers = confirm("Would you like to use numbers?");
+  // Conditional statement to check if password length is at least 8 characters long. Prompts end if this evaluates false
+  if (passLength < 8) {
+    alert('Password length must be at least 8 characters');
+    return;
+  }
+  // Conditional statement to check if password length is less than 128 characters long. Prompts end if this evaluates false
+  if (passLength > 128) {
+    alert('Password length must less than 129 characters');
+    return;
+  }
+  // Variable to store boolean regarding the inclusion of special characters
+  var specialChar = confirm(
+    'Click OK to confirm including special characters.'
+  );
+  // Variable to store boolean regarding the inclusion of numeric characters
+  var numbers = confirm(
+    'Click OK to confirm including numeric characters.'
+  );
+  // Variable to store boolean regarding the inclusion of lowercase characters
+  var lowercase = confirm(
+    'Click OK to confirm including lowercase characters.'
+  );
+  // Variable to store boolean regarding the inclusion of uppercase characters
+  var uppercase = confirm(
+    'Click OK to confirm including uppercase characters.'
+  );
+  // Creating empty array to push into?
+  var caseArray = [];
+  // Defining which part of the array to use
   if (numbers === true) {
-    array[0] = numbersDef;
-  } else if (numbers === false) {
-    array[0] = "";
+    caseArray.push(numbersDef);
   }
-  var lowercase = confirm("Would you like to use lowercase letters?");
   if (lowercase === true) {
-    array[1] = lowercaseDef;
-  } else if (lowercase === false) {
-    array[1] = "";
+    caseArray.push(lowercaseDef);
   }
-  var uppercase = confirm("Would you like to use uppercase letters?");
   if (uppercase === true) {
-    array[2] = uppercaseDef;
-  } else if (uppercase === false) {
-    array[2] = "";
+    caseArray.push(uppercaseDef);
   }
-  var specialChar = confirm("Would you like to use special characters?");
   if (specialChar === true) {
-    array[3] = specialCharDef;
-  } else if (specialChar === false) {
-    array[3] = "";
+    caseArray.push(specialCharDef);
   }
 
-  var numbersDef = '1234567890';
-  var lowercaseDef = 'abcdefghijklmnopqrstuvwxyz';
-  var uppercaseDef = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var specialCharDef = '!@#$%^&*()-_=+`~,.<>/?\|;:';
+  // THIS IS WHERE MY NEW CODE STARTS ALSO HI JAMES <3
 
-  //THIS IS  N O T  AR
-  var array = [numbersDef, lowercaseDef, uppercaseDef, specialCharDef];
-  var arrayEmpty = [];
-  var charset = (ar[0] + ar[1] + ar[2] + ar[3]);
-
-	var result = "";
-	for (var i = 0; i < len; i++)
-		result += charset[randomInt(charset.passLength)];
-	return result;
+  // Checking array, trying to make it a string so charAt works on line 123
+  console.log("caseArray is: " + caseArray);
+  caseArray.toString;
+  console.log("caseArray is now: " + caseArray)
+  // Executing the function
+  function generatePassword(length) {
+    result = '';
+      var charactersLength = caseArray.length;
+    for (var i = 0; i < length; i++) {
+       var result = caseArray.charAt(Math.floor(Math.random() * charactersLength));
+    }
+  }
+console.log(generatePassword(passLength));
 }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+var generate = document.getElementById("generate");
+var password = document.getElementById("password");
+generate.onclick = getPasswordOptions();
 
